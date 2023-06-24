2f4b3cbf38d3542b2d37942f269990772496a469
sjanedev <135636574+sjanedev@users.noreply.github.com>
2023-06-24 20:17:01 +0800

    added services and portfolio page/s

--- imagesearch/index.js
+++ imagesearch/index.js
@@ -11,39 +11,72 @@ let page = 1;
 
 async function searchImages(){
     inputData = inputEl.value;
+    // unsplash request URL
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
-
+    //fetching response from unsplash 
     const response = await fetch(url);
-    //console.log(response);
+    // console.log(response);
+    // converting response to json format
     const data = await response.json();
-    console.log(data.total);
-    const results = data.results;
+    // console.log(data.total);
+    if (data.total > 0){
 
-    if (page === 1){
-        searchResults.innerHTML = "";
+        // extracting data from json response
+        const results = data.results;
+
+        if (page === 1){
+            searchResults.innerHTML = "";
+        }
+        results.map((result) =>{
+
+            // console.log(result);    
+            // const imageWrapper = document.createElement('div');
+            // imageWrapper.classList.add("search-result");
+            // const image = document.createElement('img');
+            // image.src=result.urls.small;
+            // image.alt=result.alt_description;
+            // const imageLink= document.createElement('a');
+            // imageLink.href=result.links.html;
+            // imageLink.target="_blank";    
+            // imageLink.textContent=result.alt_description;
+
+            // imageWrapper.appendChild(image);
+            // imageWrapper.appendChild(imageLink);
+            // searchResults.appendChild(imageWrapper);
+
+            const anchorWrapper = document.createElement('a');
+            anchorWrapper.target="_blank";
+            anchorWrapper.href=result.links.html;
+            const imageWrapper = document.createElement('div');
+            imageWrapper.classList.add("search-result");
+            const image = document.createElement('img');
+            image.src=result.urls.small;
+            image.alt=result.alt_description;
+            const imageText = document.createElement('p');
+            imageText.classList.add("image-text");
+            imageText.textContent = result.alt_description;
+
+            anchorWrapper.appendChild(imageWrapper);
+            imageWrapper.appendChild(image);
+            imageWrapper.appendChild(imageText);
+            searchResults.appendChild(anchorWrapper);
+            });
+
+        page++ 
+        if(page>1){
+            showMore.style.display="block";
+        }
+        // alert(url);
     }
-    results.map((result) =>{
-        const imageWrapper = document.createElement('div');
-        imageWrapper.classList.add("search-result");
-        const image = document.createElement('img');
-        image.src=result.urls.small;
-        image.alt=result.alt_description;
-        const imageLink= document.createElement('a');
-        imageLink.href=result.links.html;
-        imageLink.target="_blank";    
-        imageLink.textContent=result.alt_description;
-
-        imageWrapper.appendChild(image);
-        imageWrapper.appendChild(imageLink);
-        searchResults.appendChild(imageWrapper);
-    });
-
-    page++ 
-    if(page>1){
-        showMore.style.display="block";
+    else{
+        searchResults.textContent = "";
+        const message = document.createElement('div');
+        message.classList.add("message");
+        message.textContent = "No results found";
+        page = 1;
+        showMore.style.display = "none"; 
+        searchResults.appendChild(message);
     }
-    // alert(url);
-
 }
 formEl.addEventListener("submit", (Event)=>{
     event.preventDefault();
