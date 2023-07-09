1ba111911e111d839c8ea4993dbaba245dd250a2
sjanedev <135636574+sjanedev@users.noreply.github.com>
2023-07-09 17:26:14 +0800

    testing another repo

--- imagesearch/config.js
+++ imagesearch/config.js
@@ -1,4 +1,4 @@
-var config = {
-    UNSPLASH_API_KEY : 'UmmPu5UsPf-XdLt_ndo42z6w8iaA9JKz2KPem6NfmMg',
+cvar config = {
+    UNSPLASH_API_KEY : 'UmmPu5UsPf-XdLt_ndo42z6w8iaA9JKz2KPem6NfmMg', 
     GOOGLE_SHEETS_URL : 'https://script.google.com/macros/s/AKfycbwg75LrXDonBda2eAYMOTxdSEVwxtxnEbZxSjiaOwUKuUEGHa2A7PmAe1kA4jhWA7oR/exec'
 }
