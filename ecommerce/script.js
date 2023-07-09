5cde48b52f02375933d6241fbb59d7766b0c0361
sjanedev <135636574+sjanedev@users.noreply.github.com>
2023-07-09 16:25:34 +0800

    added services and portfolio page/s

--- ecommerce/script.js
+++ ecommerce/script.js
@@ -12,3 +12,40 @@ if (closeIcon){
         nav.classList.remove('active');
     })
 }
+
+let mainImg = document.getElementById("MainImg");
+let smallImgs = document.getElementsByClassName("small-img");
+let proDivs = document.getElementsByClassName("pro");
+
+//
+// let shopURL = window.location.href;
+// let urlString =  new URL(shopURL);
+// let imgUrl = urlString.searchParams.get("img");
+// console.log(imgUrl)
+// if (mainImg) {
+//     mainImg.src = imgUrl;
+
+// } 
+//
+
+for (img of smallImgs) {
+    img.addEventListener('click', function(e){
+        mainImg.src = e.target.src;
+    }) 
+}
+
+for (proDiv of proDivs) {
+    proDiv.addEventListener('click', function(e){
+        window.location.href=`sproduct.html`;
+        
+    }) 
+}
+
+
+// for (proDiv of proDivs) {
+//     proDiv.addEventListener('click', function(e){
+//         imgSrc = this.firstChild.nextSibling.src;
+//         window.location.href=`sproduct.html?img=${imgSrc}`;
+        
+//     }) 
+// }
